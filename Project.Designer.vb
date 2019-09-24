1db070f4401cd59806f5a83d3777cbd01a624bca
pnowy11 <55133228+pnowy11@users.noreply.github.com>
2019-09-24 02:26:40 +0100

    First Commit

---
+++ Project.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.JoJo = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.InputBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(285, 148)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(201, 114)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Do the JoJo"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'JoJo
+        '
+        Me.JoJo.FormattingEnabled = True
+        Me.JoJo.Items.AddRange(New Object() {"JoJo 1", "JoJo 2", "Pepinga", "HORRRRYYYY SHIEEEEETTT", "OOOHHHHH MYYY GAWWWWWDD", "OOOHHHH NOOOOOOO", "OOOOHHHHH SHIIEEEEETT"})
+        Me.JoJo.Location = New System.Drawing.Point(36, 52)
+        Me.JoJo.Name = "JoJo"
+        Me.JoJo.Size = New System.Drawing.Size(216, 30)
+        Me.JoJo.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "ORRRYY SHIETTT"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(36, 110)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Add item"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'InputBox
+        '
+        Me.InputBox.Location = New System.Drawing.Point(117, 112)
+        Me.InputBox.Name = "InputBox"
+        Me.InputBox.Size = New System.Drawing.Size(100, 20)
+        Me.InputBox.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(428, 44)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Label3"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(431, 96)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.InputBox)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.JoJo)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents JoJo As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents InputBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Project.vb
@@ -0,0 +1,37 @@
+﻿
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = “alien titties”
+        Me.BackColor = Color.Cyan
+        Me.Opacity = 0.5
+        Me.Size = New Size(1920, 1080)
+        MaximizeBox = True
+        BackgroundImage = Image.FromFile("C:\Users\pnowy11\Pictures\GiornoSans.png")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("50" & "+" & "50" & "-" & "20" & "*" & "2" & "=" & 50 + 50 - 20 * 2)
+
+    End Sub
+
+    Private Sub JoJo_Protagonists_SelectedIndexChanged(sender As Object, e As EventArgs) Handles JoJo.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+    Dim myitem As String
+
+    Private Sub InputBox_TextChanged(sender As Object, e As EventArgs) Handles InputBox.TextChanged
+
+
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim counter As Integer
+        Do While counter <= 1000
+            Label3.Text = counter
+            counter += 1
+        Loop
+    End Sub
+End Class
